6b52bc4072268af89322c6c1183b5dad36b1b475
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-04-12 19:36:53 +0530

    fix: global styles

--- src/ui/layout/components/DefaultLayout.tsx
+++ src/ui/layout/components/DefaultLayout.tsx
@@ -1,16 +1,11 @@
 import { Global } from "@emotion/react";
+import { GLOBAL_STYLES } from "@ui/styles";
 import { Outlet } from "react-router";
 
 export const DefaultLayout = () => {
   return (
     <>
-      <Global
-        styles={{
-          body: {
-            margin: 0,
-          },
-        }}
-      />
+      <Global styles={GLOBAL_STYLES} />
       <Outlet />
     </>
   );
---
+++ src/ui/styles.ts
@@ -0,0 +1,5 @@
+export const GLOBAL_STYLES = {
+  body: {
+    margin: 0,
+  },
+};
