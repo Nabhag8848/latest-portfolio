b69cfcc332dcc3d47aa29c86824cf548ef97b0ab
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-04-12 20:26:37 +0530

    cache global styles

--- src/ui/layout/components/DefaultLayout.tsx
+++ src/ui/layout/components/DefaultLayout.tsx
@@ -1,10 +1,11 @@
 import { Global, useTheme } from "@emotion/react";
 import { globalStyles } from "@ui/styles/global";
+import { useMemo } from "react";
 import { Outlet } from "react-router";
 
 export const DefaultLayout = () => {
   const theme = useTheme();
-  const styles = globalStyles(theme);
+  const styles = useMemo(() => globalStyles(theme), [theme]);
 
   return (
     <>
